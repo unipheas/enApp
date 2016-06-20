9e79c70e28e8512d717b264fc006f96c4f0e9539
James <colbystuition@gmail.com>
2016-06-20 10:58:37 +0800

    getting merge errors when click pull, see what this does....

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,4 +1,5 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
+<<<<<<< Updated upstream
 module.exports = [
     {
         "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
@@ -12,6 +13,12 @@ module.exports.metadata =
 // TOP OF METADATA
 {
     "cordova-plugin-splashscreen": "3.2.2",
+=======
+module.exports = [];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+>>>>>>> Stashed changes
     "cordova-plugin-whitelist": "1.2.2"
 }
 // BOTTOM OF METADATA
