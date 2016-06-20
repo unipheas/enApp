3069e7e2616d2d192369477b84225d0ce75435ed
Brian Phillips <2313265939@qq.com>
2016-06-20 10:57:13 +0800

    update 6.20 10:56
    updating new content, don't really know what's updating because it's a bunch of new crap.. must have missed it before or something.

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,27 +1,9 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-    {
-        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
-        "id": "cordova-plugin-splashscreen.SplashScreen",
-        "clobbers": [
-            "navigator.splashscreen"
-        ]
-    },
-    {
-        "file": "plugins/ionic-plugin-keyboard/www/android/keyboard.js",
-        "id": "ionic-plugin-keyboard.keyboard",
-        "clobbers": [
-            "cordova.plugins.Keyboard"
-        ],
-        "runs": true
-    }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-splashscreen": "3.2.2",
-    "cordova-plugin-whitelist": "1.2.2",
-    "ionic-plugin-keyboard": "2.2.0"
+    "cordova-plugin-whitelist": "1.2.2"
 }
 // BOTTOM OF METADATA
 });
