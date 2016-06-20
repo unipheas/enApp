ce704ca94ec6b99a3d0c1d2317766400ab0a40cb
Brian Phillips <2313265939@qq.com>
2016-06-20 18:21:27 +0800

    testing js
    was testing js.. seems to be having problems linking a js file, had to run code inside html.. not sure why this is happening.

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,31 +1,8 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-module.exports = [
-    {
-        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
-        "id": "cordova-plugin-splashscreen.SplashScreen",
-        "clobbers": [
-            "navigator.splashscreen"
-        ]
-    }
-];
-module.exports.metadata = 
-// TOP OF METADATA
-{
-    "cordova-plugin-splashscreen": "3.2.2",
-=======
 module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
 {
->>>>>>> Stashed changes
-=======
-module.exports = [];
-module.exports.metadata = 
-// TOP OF METADATA
-{
->>>>>>> master
     "cordova-plugin-whitelist": "1.2.2"
 }
 // BOTTOM OF METADATA
