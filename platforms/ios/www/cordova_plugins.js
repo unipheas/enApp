d0ac5924848541d69987cdc3a177bad7fda61136
Brian Phillips <2313265939@qq.com>
2016-06-24 13:05:15 +0800

    body & JS
    - corrected body width so it can't move anymore.
    - linked the js file and tested, it works!

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,44 +1,7 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-module.exports = [
-    {
-        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
-        "id": "cordova-plugin-splashscreen.SplashScreen",
-        "clobbers": [
-            "navigator.splashscreen"
-        ]
-    }
-];
-module.exports.metadata = 
-// TOP OF METADATA
-{
-    "cordova-plugin-splashscreen": "3.2.2",
-=======
 module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
-{
->>>>>>> Stashed changes
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> master
-module.exports = [];
-module.exports.metadata = 
-// TOP OF METADATA
-{
-<<<<<<< HEAD
-<<<<<<< Updated upstream
->>>>>>> master
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> master
-    "cordova-plugin-whitelist": "1.2.2"
-}
+{}
 // BOTTOM OF METADATA
 });
