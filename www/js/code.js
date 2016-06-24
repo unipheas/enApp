e7f34272962236881797c29e3910333f90b82aca
Brian Phillips <2313265939@qq.com>
2016-06-24 20:11:54 +0800

    darken sub button in long vowels

--- www/js/code.js
+++ www/js/code.js
@@ -1,4 +1,5 @@
 document.getElementById("first").style.background="rgba(0,0,0,0.80)";
+document.getElementById("subFirst").style.background="rgba(0,0,0,0.80)";
 
 $(document).ready(function(){ //When document is ready run the anon function.
 
