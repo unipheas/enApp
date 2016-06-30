f37effa15490b48a78038a93e8249dadc3352db2
Brian Phillips <2313265939@qq.com>
2016-06-30 09:26:26 +0800

    added new class & id
    added new classes buttonDark & buttonLight. Also added an id to each button element. Changed both vowels to buttons for now.

--- www/js/code.js
+++ www/js/code.js
@@ -1,6 +1,19 @@
-document.getElementById("first").style.background="rgba(0,0,0,0.80)";
-document.getElementById("subFirst").style.background="rgba(0,0,0,0.80)";
+// document.getElementById("a").style.background="rgba(0,0,0,0.80)";
+// document.getElementById("subFirst").style.background="rgba(0,0,0,0.80)";
 
-$(document).ready(function(){ //When document is ready run the anon function.
+// $(document).ready(function(){ //When document is ready run function.
+//   $('button').on('click',function(){
+//     var $this = $(this);
+//     $this
+//       .siblings('button') //find siblings of clicked button
+//       .removeAttr('background','rgba(0,0,0,0.50)') // remove siblings with darkBG
+//       .end() //go back to clicked button
+//       .attr('background','rgba(0,0,0,0.80)'); //add darkBG
+//   });
+// });
 
-})
+$(document).ready(function(){
+  $('button').click(function(){
+     $(this).addClass('.buttonDark');
+  });
+});
