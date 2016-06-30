c922fc1442f29960b046cc7b7f9fc2f3e2a5a4de
Brian Phillips <2313265939@qq.com>
2016-06-30 18:39:01 +0800

    objects for short vowels
    built a few objects for short vowels.

--- www/js/code.js
+++ www/js/code.js
@@ -1,33 +1,68 @@
-// document.getElementById("a").style.background="rgba(0,0,0,0.80)";
-// document.getElementById("subFirst").style.background="rgba(0,0,0,0.80)";
-
-// $(document).ready(function(){ //When document is ready run function.
-//   $('button').on('click',function(){
-//     var $this = $(this);
-//     $this
-//       .siblings('button') //find siblings of clicked button
-//       .removeAttr('background','rgba(0,0,0,0.50)') // remove siblings with darkBG
-//       .end() //go back to clicked button
-//       .attr('background','rgba(0,0,0,0.80)'); //add darkBG
-//   });
-// });
-
+import "content.json";
 $(document).ready(function(){
+
+
   // Change all clicked elements to dark and light shading.
-  $('li').click(function(){ // When selector 'li' is clicked do the function
+  $('ul').on('click', 'li', function(){ // When selector 'li' is clicked do the function
     $('li').removeClass('buttonDark').addClass('buttonLight'); // remove dark and add light from all li
      $(this).removeClass('buttonLight') // selected li gets light
      $(this).addClass('buttonDark'); // selected li removes dark
   });
 
-  $('span').click(function(){
-     $(this)
-        .removeClass('buttonLight') // selected removes light
+  $('span').click(function(){ // for sub buttons in longVowels
+     $(this).removeClass('buttonLight') // selected removes light
         .addClass('buttonDark') // selected adds dark
         .siblings(this) // choose all other siblings of selected
         .removeClass('buttonDark') // remove dark
         .addClass('buttonLight'); // add light
   });
 
-  
+  // dd.fiter(':nth-child(n+4)').addClass('className'); // used to hide all except first one
+  // $('<p></p>',{
+      // text: 'Ant',
+      // class: 'vowelWord'
+  // }).appendTo('.content'); // creates the html element needed and puts at the bottom.
+
+  var shortVowelA = {
+    "img": "img/apple.png"
+    "word1": "Ant",
+    "word2": "Apple",
+    "word3": "Antelope",
+    "audio": ""
+  }
+
+  var shortVowelE = {
+    "img": "img/apple.png"
+    "word1": "Egg",
+    "word2": "End",
+    "word3": "Engine",
+    "audio": ""
+  }
+
+  var shortVowelI = {
+    "img": "img/apple.png"
+    "word1": "In",
+    "word2": "Inside",
+    "word3": "Internet",
+    "audio": ""
+  }
+
+  var shortVowelO = {
+    "img": "img/apple.png"
+    "word1": "On",
+    "word2": "Object",
+    "word3": "Ostrich",
+    "audio": ""
+  }
+  var shortVowelU = {
+    "img": "img/apple.png"
+    "word1": "Up",
+    "word2": "Under",
+    "word3": "Ugly",
+    "audio": ""
+  }
+
+  function fillContent(letter){
+    
+  }
 });
