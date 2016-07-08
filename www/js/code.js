2226a19e1ad5ce68b6d58380bf10a8b27e0b1208
Brian Phillips <2313265939@qq.com>
2016-07-08 12:55:30 +0800

    removed jquery ui & mobile, made dialog box

--- www/js/code.js
+++ www/js/code.js
@@ -739,5 +739,8 @@ $(document).ready(function(){
           .addClass('buttonLight');
   });
 
-
+  function overlay() {
+	   el = document.getElementById("overlay");
+	    el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+  }
  });
