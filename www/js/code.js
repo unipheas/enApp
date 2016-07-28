9feb2589cf4734c3ff8329eca65b02b8366d11c6
Brian Phillips <2313265939@qq.com>
2016-07-29 04:06:08 +0800

    Finished function for rest of audio. YOU CAN CLICK THE WORDS NOW!!!!!!!!

--- www/js/code.js
+++ www/js/code.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function(){
 
-  function shortVowel(picture, word1, word2, word3, audio){
+  function shortVowel(picture, word1, word2, word3, audio, wa1, wa2, wa3){
     $('.content img').replaceWith($('<img></img>',{
       src: 'img/phonics_img/shortVowels/'+picture
     }));
@@ -10,16 +10,25 @@ $(document).ready(function(){
       id: 'word1',
       text: word1
     }));
+    $('#word1').on('click', function(){
+      new Audio("../audio/short_vowels/mp3/"+wa1).play();
+    });
     $('.content p#word2').replaceWith($('<p></p>',{
       class: 'vowelWord',
       id: 'word2',
       text: word2
     }));
+    $('#word2').on('click', function(){
+      new Audio("../audio/short_vowels/mp3/"+wa2).play();
+    });
     $('.content p#word3').replaceWith($('<p></p>',{
       class: 'vowelWord',
       id: 'word3',
       text: word3
     }));
+    $('#word3').on('click', function(){
+      new Audio("../audio/short_vowels/mp3/"+wa3).play();
+    });
     $('.content audio').replaceWith($('<audio></audio>',{
       id: "mp3",
       autoplay: "autoplay",
@@ -28,8 +37,8 @@ $(document).ready(function(){
     }));
     document.getElementById('mp3').play();
   }
-  
-  function longVowel(pic, word1, word2, word3, audio, tag){
+
+  function longVowel(pic, word1, word2, word3, audio, tag, wa1, wa2, wa3){
     $('.content img').replaceWith($('<img></img>',{
       src: 'img/phonics_img/longVowels/'+pic
     }));
@@ -38,16 +47,25 @@ $(document).ready(function(){
       id: 'word1',
       text: word1
     }));
+    $('#word1').on('click', function(){
+      new Audio("../audio/long_vowels/mp3/"+wa1).play();
+    });
     $('.content p#word2').replaceWith($('<p></p>',{
       class: 'vowelWord',
       id: 'word2',
       text: word2
     }));
+    $('#word2').on('click', function(){
+      new Audio("../audio/long_vowels/mp3/"+wa2).play();
+    });
     $('.content p#word3').replaceWith($('<p></p>',{
       class: 'vowelWord',
       id: 'word3',
       text: word3
     }));
+    $('#word3').on('click', function(){
+      new Audio("../audio/long_vowels/mp3/"+wa3).play();
+    });
     $('.content audio').replaceWith($('<audio></audio>',{
       id: "mp3",
       autoplay: "autoplay",
@@ -105,112 +123,111 @@ $(document).ready(function(){
 
           // This is for the SHORT VOWELS
   $('#shortA').on('click',function(){
-    shortVowel('apple.png','Apple 苹果','Ant 蚂蚁','Antelope 羚羊','a.mp3');
+    shortVowel('apple.png','Apple 苹果','Ant 蚂蚁','Antelope 羚羊','a.mp3','apple.mp3','ant.mp3','antelope.mp3');
   });
   $('#shortE').on('click',function(){
-    shortVowel('egg.png','Egg 鸡蛋','End 结束','Engine 引擎','e.mp3');
+    shortVowel('egg.png','Egg 鸡蛋','End 结束','Engine 引擎','e.mp3','egg.mp3','end.mp3','engine.mp3');
   });
   $('#shortI').on('click',function(){
-    shortVowel('insect.png', 'Insect 昆虫', 'In 在...里面', 'Internet 因特网', 'i.mp3');
+    shortVowel('insect.png', 'Insect 昆虫', 'In 在...里面', 'Internet 因特网', 'i.mp3','insect.mp3','in.mp3','internet.mp3');
   });
   $('#shortO').on('click',function(){
-    shortVowel('orange.png','Orange 橙子','Object 物体','Ostrich 鸵鸟','o.mp3');
+    shortVowel('orange.png','Orange 橙子','Object 物体','Ostrich 鸵鸟','o.mp3','orange.mp3','object.mp3','ostrich.mp3');
   });
   $('#shortU').on('click',function(){
-    shortVowel('umbrella.png','Umbrella 雨伞','Under 在...下面','Ugly 丑','u.mp3');
+    shortVowel('umbrella.png','Umbrella 雨伞','Under 在...下面','Ugly 丑','u.mp3','umbrella.mp3','under.mp3','ugly.mp3');
   });
 
         // This is for the LONG VOWELS
-  $('.subVmenu').hide();
-  $('.subA').show();
+  function fadeButton(target){
+    $('.subVmenu').hide();
+    $(target).show();
+  }
+  fadeButton('.subA');
 
           // To show content in LONG VOWELS
                 //Long Vowel A
   $('#longA').on('click',function(){
-    $('.subVmenu').hide();
-    $('.subA').show();
-    longVowel('day.png','Day 白天','Pay 付钱','Play 玩耍','a.mp3', 'span#ay');
+    fadeButton('.subA');
+    longVowel('day.png','Day 白天','Pay 付钱','Play 玩耍','a.mp3', 'span#ay', 'day.mp3', 'pay.mp3', 'play.mp3');
   });
   $('#ay').on('click',function(){
-    longVowel('day.png','Day 白天','Pay 付钱','Play 玩耍','a.mp3','#ay');
+    longVowel('day.png','Day 白天','Pay 付钱','Play 玩耍','a.mp3','#ay', 'day.mp3', 'pay.mp3', 'play.mp3');
   });
   $('#ai').on('click',function(){
-    longVowel('rain.png','Rain 下雨','Laid 下蛋','Paid 付钱','a.mp3','#ai');
+    longVowel('rain.png','Rain 下雨','Laid 下蛋','Paid 付钱','a.mp3','#ai','rain.mp3','laid.mp3','paid.mp3');
   });
   $('#a_e').on('click',function(){
-    longVowel('plane.png','Plane 飞机','Game 游戏','Paper 纸','a.mp3','#a_e');
+    longVowel('plane.png','Plane 飞机','Game 游戏','Paper 纸','a.mp3','#a_e','plane.mp3','game.mp3','paper.mp3');
   });
               //Long Vowel E
   $('#longE').on('click',function(){
-    $('.subVmenu').hide();
-    $('.subE').show();
-    longVowel('bee.png','Bee 蜜蜂','Free 免费','Tree 树','e.mp3','span#ee');
+    fadeButton('.subE');
+    longVowel('bee.png','Bee 蜜蜂','Free 免费','Tree 树','e.mp3','span#ee','bee.mp3','free.mp3','tree.mp3');
   });
   $('#ee').on('click',function(){
-    longVowel('bee.png','Bee 蜜蜂','Free 免费','Tree 树','e.mp3','#ee');
+    longVowel('bee.png','Bee 蜜蜂','Free 免费','Tree 树','e.mp3','#ee','bee.mp3','free.mp3','tree.mp3');
   });
   $('#ea').on('click',function(){
-    longVowel('eat.png','Eat 吃','Clean 干净','Flea 跳蚤','e.mp3','#ea');
+    longVowel('eat.png','Eat 吃','Clean 干净','Flea 跳蚤','e.mp3','#ea','eat.mp3','clean.mp3','flea.mp3');
   });
   $('#e_e').on('click',function(){
-    longVowel('chinese.png','Chinese 中国人','Athlete 运动员','Theme 主题','e.mp3','#e_e');
+    longVowel('chinese.png','Chinese 中国人','Athlete 运动员','Theme 主题','e.mp3','#e_e','chinese.mp3','athlete.mp3','theme.mp3');
   });
   $('#eay').on('click',function(){
-    longVowel('angry.png','Angry 生气','Happy 高兴','Lady 女士','e.mp3','#eay');
+    longVowel('angry.png','Angry 生气','Happy 高兴','Lady 女士','e.mp3','#eay','angry.mp3','happy.mp3','lady.mp3');
   });
             //Long Vowel I
   $('#longI').on('click',function(){
-    $('.subVmenu').hide();
-    $('.subI').show();
-    longVowel('pie.png','Pie 派','Lie 撒谎','Tie 领带','i.mp3','span#ie');
+    fadeButton('.subI');
+    longVowel('pie.png','Pie 派','Lie 撒谎','Tie 领带','i.mp3','span#ie','pie.mp3','lie.mp3','tie.mp3');
   });
   $('#ie').on('click',function(){
-    longVowel('pie.png','Pie 派','Lie 撒谎','Tie 领带','i.mp3','span#ie');
+    longVowel('pie.png','Pie 派','Lie 撒谎','Tie 领带','i.mp3','span#ie','pie.mp3','lie.mp3','tie.mp3');
   });
   $('#igh').on('click',function(){
-    longVowel('light.png','Light 轻','High 高','Sigh 叹气','i.mp3','#igh');
+    longVowel('light.png','Light 轻','High 高','Sigh 叹气','i.mp3','#igh','light.mp3','high.mp3','sigh.mp3');
   });
   $('#y').on('click',function(){
-    longVowel('fly.png','Fly 苍蝇','Fry 炸','Cry 哭','i.mp3','#y');
+    longVowel('fly.png','Fly 苍蝇','Fry 炸','Cry 哭','i.mp3','#y','fly.mp3','fry.mp3','cry.mp3');
   });
   $('#i_e').on('click',function(){
-    longVowel('bike.png','Bike 自行车','Time 时间','Wine 红酒','i.mp3','#i_e');
+    longVowel('bike.png','Bike 自行车','Time 时间','Wine 红酒','i.mp3','#i_e','bike.mp3','time.mp3','wine.mp3');
   });
           //Long Vowel O
   $('#longO').on('click',function(){
-    $('.subVmenu').hide();
-    $('.subO').show();
-    longVowel('boat.jpg','Boat 船','Road 道路','Coat 外套','o.mp3','span#oa');
+    fadeButton('.subO');
+    longVowel('boat.jpg','Boat 船','Road 道路','Coat 外套','o.mp3','span#oa','boat.mp3','road.mp3','coat.mp3');
   });
   $('#oa').on('click',function(){
-    longVowel('boat.jpg','Boat 船','Road 道路','Coat 外套','o.mp3','span#oa');
+    longVowel('boat.jpg','Boat 船','Road 道路','Coat 外套','o.mp3','span#oa','boat.mp3','road.mp3','coat.mp3');
   });
   $('#ow').on('click',function(){
-    longVowel('window.png','Window 窗户','Blow 吹','Row 划船','o.mp3','#ow');
+    longVowel('window.png','Window 窗户','Blow 吹','Row 划船','o.mp3','#ow','window.mp3','blow.mp3','row.mp3');
   });
   $('#o_e').on('click',function(){
-    longVowel('phone.png','Phone 手机','Bone 骨头','Tone 声调','o.mp3','#o_e');
+    longVowel('phone.png','Phone 手机','Bone 骨头','Tone 声调','o.mp3','#o_e','phone.mp3','bone.mp3','tone.mp3');
   });
           //Long Vowel U
   $('#longU').on('click',function(){
-    $('.subVmenu').hide();
-    $('.subU').show();
-    longVowel('blue.png','Blue 蓝色','Argue 争吵','True 真实','u.mp3','span#ue');
+    fadeButton('.subU');
+    longVowel('blue.png','Blue 蓝色','Argue 争吵','True 真实','u.mp3','span#ue','blue.mp3','argue.mp3','true.mp3');
   });
   $('#ue').on('click',function(){
-    longVowel('blue.png','Blue 蓝色','Argue 争吵','True 真实','u.mp3','span#ue');
+    longVowel('blue.png','Blue 蓝色','Argue 争吵','True 真实','u.mp3','span#ue','blue.mp3','argue.mp3','true.mp3');
   });
   $('#ew').on('click',function(){
-    longVowel('mew.png','Mew 喵喵叫','Blew 吹','Shrew 鼩鼱','u.mp3','#ew');
+    longVowel('mew.png','Mew 喵喵叫','Blew 吹','Shrew 鼩鼱','u.mp3','#ew','mew.mp3','blew.mp3','shrew.mp3');
   });
   $('#u_e').on('click',function(){
-    longVowel('cube.png','Cube 方块','Refuse 拒绝','Tube 管道','u.mp3','#u_e');
+    longVowel('cube.png','Cube 方块','Refuse 拒绝','Tube 管道','u.mp3','#u_e','cube.mp3','refuse.mp3','tube.mp3');
   });
 
             // DIALOG BOX FOR CONTENT
   $('#close').on('click',function(){
     $('#overlay').hide();
   });
+
   $('#overlay').hide();
 
               // CONSONANTS
